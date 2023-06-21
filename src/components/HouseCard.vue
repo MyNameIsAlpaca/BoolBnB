cc96b9d54085b99c2d54973f166ea2b2bde732b1
SimoneFerrini <ferrini.simone4@gmail.com>
2023-06-21 12:24:27 +0200

    feat: card e home

---
+++ src/components/HouseCard.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+    name: 'HouseCard',
+
+    data() {
+        return {
+            
+        }
+    },
+
+    props: {
+        house: Object,
+    },
+
+    computed: {
+
+        shortContent() {
+            if (this.house.description.length > 30) {
+                return this.house.description.substring(0, 30) + '...';
+            } else {
+                return this.house.description;
+            }
+        },
+
+    }
+}
+</script>
+
+<template>
+    <div class="card" style="width: 18rem;">
+            <img :src="house.thumbnail" class="card-img-top" alt="Immagine di copertina">
+            <div class="card-body">
+              <h5 class="card-title">{{house.title}}</h5>
+              <p class="card-text">{{house.description}}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">N° stanze: {{house.rooms}}</li>
+              <li class="list-group-item">N° bagni: {{house.bathrooms}}</li>
+              <li class="list-group-item">m&sup2;: {{house.square_mt}}</li>
+              <li class="list-group-item">Servizi: 
+                  <i v-for="singleService in services" :class="singleService.icon"></i> {{singleService.name}}
+              </li>
+            </ul>
+
+            
+            <!-- <div class="card-body d-flex gap-3 align-items-center">
+              <div class="container d-flex align-items-center gap-2">
+                <button class="btn btn-secondary"><a href="{{route('houses.show', $house)}}" class="link-light">Guarda la tua casa</a></button> 
+              </div>
+              <form action="{{ route('visibility.index', $house) }}" method="POST">
+                @csrf
+                <div class="form-check form-switch">
+                  <input name="visibility" class="form-check-input submitCheckbox" type="checkbox" role="switch" {{  $house->visibility ? "checked" : "" }}>
+                </div>
+              </form>
+            </div> -->
+          </div>
+</template>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,20 +1,54 @@
 <script>
-import AppNavSearch from '../partials/AppNavSearch.vue';
-export default {
-  name: 'AppHome',
-  data() {
-    return {
+import axios from 'axios';
 
+import HouseCard from './components/HouseCard.vue';
+
+export default{
+  name: 'AppHome',
+
+  data(){
+    return{
+      apiUrl: 'http://127.0.0.1:8000/api/houses',
+
+      houses: [],
+
+      
     }
   },
-  components: {
-    AppNavSearch,
-  }
+
+  components:{
+    HouseCard,
+  },  
+
+  created() {
+    this.getHouses(this.apiUrl);
+  },
+
+  methods:{
+    getHouses(apiUrl){
+      axios.get(apiUrl).then(res =>{
+        console.log(res);
+        
+        this.houses = res.data.results.data;
+      });
+    }
+  },
 }
 </script>
 
 <template>
-  
+  <h1 class="text-center">In evidenza: </h1>
+  <div class="text-center w-75 mx-auto d-flex justify-content-between row">
+    <div v-for="house in houses" class="col-4 mb-5">
+      <HouseCard :house="house"></HouseCard>
+    </div>
+
+    <!-- <div class="project-navigation">
+      <button v-for="link in pagination.links" class="btn" v-html="link.label" :class="link.active ? 'btn-danger' : 'btn-outline-danger'"
+        :disabled="link.url == null ? true : false" @click="getProjects(link.url)">
+        
+      </button>-->
+    </div> 
 </template>
 
 <style scoped lang="scss">
